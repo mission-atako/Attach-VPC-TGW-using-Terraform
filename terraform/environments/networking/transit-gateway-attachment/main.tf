9614123dd924273d6e1e107470ab310c33a45ef5
Arnaud Tako <atako@missioncloud.com>
2025-10-03 15:04:54 -0400

    terraform restructured2

--- terraform/environments/networking/transit-gateway-attachment/main.tf
+++ terraform/environments/networking/transit-gateway-attachment/main.tf
@@ -1,11 +1,11 @@
 module "key" {
-    
-    source = "../../../modules/transit-gateway-attachment"
+
+    source = "../../../modules/tgw-attachment"
     region = var.region
     name   = var.name
     tgw_id = var.tgw_id
-    vpc_id = var.vpc
-    subnet_ids = var.subnet
+    vpc_id = var.vpc_id
+    subnet_ids = var.subnet_ids
     vpc_route_table_ids = var.vpc_route_table_ids
     remote_cidrs = var.remote_cidrs
 }
